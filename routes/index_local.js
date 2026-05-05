4d0f2ca5b86099eaa308b71a76845a69bbe64570
Henrique Pereira <henrique4771@gmail.com>
2026-05-05 20:09:01 -0300

    ajustando api de presenca por nome

--- routes/index_local.js
+++ routes/index_local.js
@@ -125,10 +125,10 @@ app.get("/api/presenca", async (req, res) => {
       objPersenca = { dataPresenca: { $gte: dataDe, $lte: dataAte } }
       presenca = await db.find("presenca", objPersenca);
     } else if (!dataDe && nomeAluno){
-      objPersenca = { dataPresenca: { $regex: nomeAluno } }
+      objPersenca = { nomeAluno: { $regex: nomeAluno } }
       presenca = await db.find("presenca", objPersenca);
     } else if (!dataDe && nomeAluno){
-       objPersenca = { dataPresenca: { $gte: dataDe, $lte: dataAte, $regex: nomeAluno  } }
+       objPersenca = { dataPresenca: { $gte: dataDe, $lte: dataAte  }, nomeAluno:{$regex: nomeAluno}}
        presenca = await db.find("presenca", objPersenca);
     }
 
